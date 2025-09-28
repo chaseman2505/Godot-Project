d0afe4ae8b93e0158112e68ebefb9c7012bfb29d
chaseman2505 <chaseman2505@gmail.com>
2025-09-28 11:52:27 -0400

    Added some functionality. Sprite2D will rotate when loaded and when W is pressed.

--- test-2/Sprite2d.cs
+++ test-2/Sprite2d.cs
@@ -3,5 +3,22 @@ using System;
 
 public partial class Sprite2d : Sprite2D
 {
+	//Notes: the root node will always get saved as a scene when you eventually hit save
+	//scene or save all scenes. This scene which is based on the root node cannot be instanced.
+	
+	
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+		this.Rotate(3);
+	}
 
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+		if (Input.IsPhysicalKeyPressed(Key.W))
+		{
+			this.Rotate(0.001f);
+		}
+	}
 }
